55c76b5ba9bb2dd87824201be05f00d4e764754d
Mario Hero <mariocanfly@126.com>
2021-11-05 16:27:44 +0800

    Update toolUnRar.py

--- toolUnRar.py
+++ toolUnRar.py
@@ -7,14 +7,16 @@
 import sys
 import os
 import subprocess
+import datetime
 
 # you can change it >>>>>
 
-PASSWD     = ["hello","123456"]  # the possible passwords
-DELETEIT   = False                                      # DANGER!! If it is True,will delete rar file after extraction
+PASSWD     = ["hello","6666"]  # the possible passwords
+DELETEIT   = False                                      # 解压后删除压缩包 DANGER!! If it is True,will delete rar file after extraction
 LOC_WINRAR = "C:\\Program Files\\WinRAR\\"              # location of WinRAR
 LOC_7Z     = "C:\\Program Files\\7-Zip\\"               # location of 7-Zip
-SAVE_MODE  = True                                       # if the suffix of file doesn't look like a compressed file, then do nothing with it.
+SAVE_MODE  = True                                       # 如果文件后缀看上去不像压缩文件，就不解压，除非它是唯一选择的文件 if the extension name of file doesn't look like a compressed file, then do nothing with it.
+MULTI_UNRAR = DELETEIT and True                         # 解压双重压缩文件 unzip double compressed files
 
 # <<<<< you can change it
 
@@ -28,15 +30,22 @@ NOT_RAR_FILE = ["jpg","exe","png","mkv","mp4","mp3","avi","mov","jpeg","wav","gi
 ENABLE_RAR = False         # initial state only
 ENABLE_7Z = False          # initial state only
 
+RENAME_UNRAR = True        # 防止解压的文件与压缩包重名 In order to prevent that the decompressed file has the same name with current file, we rename the compressed file first
+
 # for guessing >>>
-GUESS_FLAG_INIT     = ["密码", "码", "password", "Password"]    #0
+GUESS_FLAG_INIT     = ["密码", "码", "password", "Password"]  #0
 GUESS_FLAG_START_1  = [":", "："]                            #1
-GUESS_FLAG_START_2  = ["是", "为", "is", "are"," "]          #1
+GUESS_FLAG_START_2  = ["是", "为", "is", "are"," "]           #1
 GUESS_FLAG_END      = ["\n","   "]                           #2
-GUESS_FLAG_DIVIDE   = ["或是", "或", " or "]                 #3
+GUESS_FLAG_DIVIDE   = ["或是", "或", " or "]                  #3
 # <<< for guessing
 
 ERROR_LIST = ""
+workSpace = ""
+lastFileName = ""
+lastFileSize = 0
+lastSpaceFiles = {}
+newSpaceFiles = {}
 
 
 def logError(comment):
@@ -177,18 +186,25 @@ def getPWFromFolder(file):
 
 def isCompressedFile(file):
     if "." in file:
-        file = file.lower()
+        fileExtension = file[file.rindex('.')+1:].lower()
         for rar in RAR_FILE:
-            if file.endswith("." + rar):
+            if fileExtension == rar:
                 return True
+        if "删" in fileExtension:
+            return True
         for media in NOT_RAR_FILE:
-            if file.endswith("." + media):
+            if fileExtension == media:
                 return False
     return not SAVE_MODE
 
 
-def utfIsNumber(uchar):
-    return uchar >= u'\u0030' and uchar<=u'\u0039'
+def fileRename(file, n):
+    pathName, fileName = os.path.split(file)
+    if '.' in fileName:
+        cutPos = fileName.rfind('.')
+        return os.path.join(pathName, fileName[:cutPos] + '(' + str(n) + ')' + fileName[cutPos:])
+    else:
+        return os.path.join(pathName, fileName + '(' + str(n) + ')')
 
 
 def winRarDo(folder, file, wd):
@@ -225,11 +241,18 @@ def unrarFile(folder, file):
     fileNameEncrypted = True
     if not folder:
         folder, file = os.path.split(file)
-        #cutPos = file.rindex("\\")
-        #folder = file[:cutPos]
-        #file = file[cutPos+1:]
-        #print(folder)
-        #print(file)
+    originalName = file
+    if RENAME_UNRAR:
+        if file.rfind('.'):
+            fileExtension = file[file.rindex('.'):]
+        else:
+            fileExtension = ''
+        dt_ms = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
+        while os.path.exists(os.path.join(folder, dt_ms + fileExtension)):
+            dt_ms = dt_ms + '(1)'
+        os.rename(os.path.join(folder, file), os.path.join(folder, dt_ms + fileExtension))
+        file = dt_ms + fileExtension
+
     if ENABLE_RAR and file.endswith(".rar"):
         winRarReturn = winRarDo(folder, file, PASSWD[0])
         #print(winRarReturn)
@@ -268,22 +291,21 @@ def unrarFile(folder, file):
                                     break
                                 else:
                                     break
-            if successThisFile:
-                return True
-            for index in range(1,len(PASSWD)):
-                winRarReturn = winRarDo(folder, file, PASSWD[index])
-                if winRarReturn == 1:
-                    continue
-                elif winRarReturn == 0:
-                    successThisFile = True
-                    PASSWD[0],PASSWD[index]=PASSWD[index],PASSWD[0]
-                    break
-                elif winRarReturn == 2:
-                    break
-                else:
-                    break
+            if not successThisFile:
+                for index in range(1,len(PASSWD)):
+                    winRarReturn = winRarDo(folder, file, PASSWD[index])
+                    if winRarReturn == 1:
+                        continue
+                    elif winRarReturn == 0:
+                        successThisFile = True
+                        PASSWD[0],PASSWD[index]=PASSWD[index],PASSWD[0]
+                        break
+                    elif winRarReturn == 2:
+                        break
+                    else:
+                        break
             
-    if not successThisFile:
+    elif not successThisFile:
         if ENABLE_7Z:
             for index in range(len(PASSWD)):
                 z7Return = z7Do(folder, file, PASSWD[index])
@@ -297,30 +319,64 @@ def unrarFile(folder, file):
                     PASSWD[0],PASSWD[index]=PASSWD[index],PASSWD[0]
                     break
                      
-    if not successThisFile: 
-        logError("No passsword for: "+file)
-    return successThisFile
+    if not successThisFile:
+        if RENAME_UNRAR:
+            os.rename(os.path.join(folder, file), os.path.join(folder, originalName))
+        logError("No passsword for: " + originalName)
+    else:
+        if DELETEIT:
+            os.remove(os.path.join(folder, file))
+            multiUnrar()
+        elif RENAME_UNRAR:
+            moveTemp = os.path.join(folder, originalName)
+            originalPath = moveTemp
+            i = 0
+            while os.path.exists(moveTemp):
+                i = i + 1
+                moveTemp = fileRename(originalPath, i)
+            os.rename(os.path.join(folder, file), moveTemp)
 
 
-def unrar(folder):
-    if os.path.isdir(folder):
-        print(folder)
-        file_list = os.listdir(folder)
-        for file in file_list:
-            if os.path.isdir(os.path.join(folder, file)):
-                #print(folder +"/"+ file)
-                #unrar(folder +"/"+file)
+def multiUnrar():
+    global workSpace, lastFileName, lastFileSize, lastSpaceFiles, newSpaceFiles
+    if MULTI_UNRAR:
+        newSpaceFiles = os.listdir(workSpace)
+        for file in newSpaceFiles:
+            if os.path.isdir(os.path.join(workSpace, file)):
                 pass
             else:
                 if isCompressedFile(file):
-                    if unrarFile(folder, file):
-                        if DELETEIT:
-                            os.remove(os.path.join(folder, file))
+                    if file not in lastSpaceFiles or (file == lastFileName and DELETEIT):
+                        newFileSize = os.path.getsize(os.path.join(workSpace, file))
+                        if newFileSize*1.3 > lastFileSize:
+                            unrarFile(workSpace, file)
+                            break
+
+
+def unrar(folder):
+    global workSpace, lastFileName, lastFileSize, lastSpaceFiles
+    if os.path.isdir(folder):
+        print(folder)
+        workSpace = folder
+        file_list = os.listdir(folder)
+        lastSpaceFiles = file_list
+        for file in file_list:
+            if os.path.isdir(os.path.join(folder, file)):
+                pass
+            else:
+                if isCompressedFile(file):
+                    lastFileName = file
+                    lastFileSize = os.path.getsize(os.path.join(folder, file))
+                    unrarFile(folder, file)
     else:
         if isCompressedFile(folder):
-            if unrarFile("", folder):
-                if DELETEIT:
-                    os.remove(folder)
+            workSpace = os.path.split(folder)[0]
+            lastFileName = os.path.split(folder)[1]
+            lastFileSize = os.path.getsize(folder)
+            lastSpaceFiles = os.listdir(workSpace)
+            unrarFile("", folder)
+
+
                   
 
 if __name__ == '__main__':
@@ -354,7 +410,12 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         while len(PASSWD) < 2:
             PASSWD.append("0")
+        oldPasswordLength = len(PASSWD)
         getPWFromFolder(sys.argv[1])
+        newPasswordLength = len(PASSWD)
+        addWD = newPasswordLength - oldPasswordLength
+        if addWD > 8:
+            PASSWD = PASSWD[addWD:] + PASSWD[:addWD]
         if len(sys.argv) == 2 and os.path.isfile(sys.argv[1]):
             SAVE_MODE = False
         # print(PASSWD)
